d4d7d466cfbbd83c6daf6cbe8ccdfa71425b110d
cp-20 <n.naoki.1587@gmail.com>
2024-11-19 21:16:36 +0900

    🍆 add command addRandomFoodmoji

--- src/extension.ts
+++ src/extension.ts
@@ -1,77 +1,93 @@
 import * as vscode from 'vscode';
-import { GitExtension, Repository } from './git';
+import { Git, GitExtension, Repository } from './git';
 import Foodmoji from './foodmoji';
 
-export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.showFoodmoji', (uri?) => {
-        const git = getGitExtension();
+const getConfigs = () => ({
+    addCustomEmoji: (vscode.workspace.getConfiguration().get('foodmoji.addCustomEmoji') || []) as { code: string; emoji: string; description: string }[],
+    onlyUseCustomEmoji: vscode.workspace.getConfiguration().get('foodmoji.onlyUseCustomEmoji') as boolean | undefined,
+    showEmojiCode: vscode.workspace.getConfiguration().get('foodmoji.showEmojiCode') as boolean | undefined,
+    outputType: vscode.workspace.getConfiguration().get('foodmoji.outputType') as 'emoji' | 'code',
+    asSuffix: vscode.workspace.getConfiguration().get('foodmoji.asSuffix') as boolean,
+});
 
-        if (!git) {
-            vscode.window.showErrorMessage('Unable to load Git Extension');
-            return;
-        }
+const getEmojis = () => {
+    const { addCustomEmoji: customEmojis, onlyUseCustomEmoji } = getConfigs();
+    if (onlyUseCustomEmoji) {
+        return [...customEmojis];
+    } else {
+        return [...Foodmoji, ...customEmojis];
+    }
+};
 
-        let addCustomEmoji: Array<any> = vscode.workspace.getConfiguration().get('foodmoji.addCustomEmoji') || [];
+export const activate = (context: vscode.ExtensionContext) => {
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.showFoodmoji', (uri?) => {
+            const emojis = getEmojis();
 
-        const showEmojiCode: boolean | undefined = vscode.workspace.getConfiguration().get('foodmoji.showEmojiCode');
+            const { showEmojiCode } = getConfigs();
+            const items = emojis.map(emojiObj => {
+                const { description, code, emoji } = emojiObj;
+                const displayCode = showEmojiCode ? code : '';
+                const label = `${emoji} ${description} ${displayCode}`;
+                return {
+                    label,
+                    code,
+                    emoji,
+                };
+            });
 
-        let emojis = [];
-        let onlyUseCustomEmoji: boolean | undefined = vscode.workspace.getConfiguration().get('foodmoji.onlyUseCustomEmoji');
-
-        if (onlyUseCustomEmoji === true) {
-            emojis = [...addCustomEmoji];
-        } else {
-            emojis = [...Foodmoji, ...addCustomEmoji];
-        }
-
-        const items = emojis.map(emojiObj => {
-            const { description, code, emoji } = emojiObj;
-            const displayCode = showEmojiCode ? code : '';
-            const label = `${emoji} ${description} ${displayCode}`;
-            return {
-                label,
-                code,
-                emoji,
-            };
-        });
-
-        vscode.window.showQuickPick(items).then(function (selected) {
-            if (selected) {
+            vscode.window.showQuickPick(items).then(selected => {
+                if (!selected) return;
                 vscode.commands.executeCommand('workbench.view.scm');
-                const outputType = vscode.workspace.getConfiguration().get('foodmoji.outputType');
-                const valueToAdd = outputType === 'emoji' ? selected.emoji : selected.code;
-                const asSuffix: boolean | undefined = vscode.workspace.getConfiguration().get('foodmoji.asSuffix') || false;
+                updateCommitWithEmoji(uri, selected);
+            });
+        }),
+        vscode.commands.registerCommand('extension.addRandomFoodmoji', (uri?) => {
+            const emojis = getEmojis();
+            const randomIndex = Math.floor(Math.random() * emojis.length);
+            const randomEmoji = emojis[randomIndex];
+            vscode.commands.executeCommand('workbench.view.scm');
+            updateCommitWithEmoji(uri, randomEmoji);
+        })
+    );
+};
 
-                if (uri) {
-                    const uriPath = uri._rootUri?.path || uri.rootUri.path;
-                    let selectedRepository = git.repositories.find(repository => repository.rootUri.path === uriPath);
-                    if (selectedRepository) {
-                        updateCommit(selectedRepository, valueToAdd, asSuffix);
-                    }
-                } else {
-                    for (let repo of git.repositories) {
-                        updateCommit(repo, valueToAdd, asSuffix);
-                    }
-                }
-            }
-        });
-    });
+const updateCommitWithEmoji = (uri: any, emoji: { emoji: string; code: string }) => {
+    const git = getGitExtension();
 
-    context.subscriptions.push(disposable);
-}
+    if (!git) {
+        vscode.window.showErrorMessage('Unable to load Git Extension');
+        return;
+    }
 
-function updateCommit(repository: Repository, valueOfFoodmoji: String, asSuffix: boolean) {
+    const { outputType, asSuffix } = getConfigs();
+    const valueToAdd = outputType === 'emoji' ? emoji.emoji : emoji.code;
+
+    if (uri) {
+        const uriPath = uri._rootUri?.path || uri.rootUri.path;
+        let selectedRepository = git.repositories.find(repository => repository.rootUri.path === uriPath);
+        if (selectedRepository) {
+            updateCommit(selectedRepository, valueToAdd, asSuffix);
+        }
+    } else {
+        for (let repo of git.repositories) {
+            updateCommit(repo, valueToAdd, asSuffix);
+        }
+    }
+};
+
+const updateCommit = (repository: Repository, valueOfFoodmoji: String, asSuffix: boolean) => {
     if (!asSuffix) {
         repository.inputBox.value = `${valueOfFoodmoji} ${repository.inputBox.value}`;
     } else {
         repository.inputBox.value = `${repository.inputBox.value} ${valueOfFoodmoji}`;
     }
-}
+};
 
-function getGitExtension() {
+const getGitExtension = () => {
     const vscodeGit = vscode.extensions.getExtension<GitExtension>('vscode.git');
     const gitExtension = vscodeGit && vscodeGit.exports;
     return gitExtension && gitExtension.getAPI(1);
-}
+};
 
-export function deactivate() {}
+export const deactivate = () => {};
